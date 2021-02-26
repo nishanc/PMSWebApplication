b932ebcbbae852e5b825d99cae6ed3826c317e3b
Nishan Wickramarathna <nishanw@99x.io>
2021-02-26 22:09:34 +0530

    first commit

---
+++ PMSWebApplication/Models/DomainModels/Payment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PMSWebApplication.Models.DomainModels
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public string TaskId { get; set; }
+        public virtual Task Task { get; set; }
+
+        public decimal? PaymentAmount { get; set; }
+        public string PayMethod { get; set; }
+    }
+}
